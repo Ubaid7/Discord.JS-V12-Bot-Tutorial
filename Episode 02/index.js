a207b46aa6a982ae25fd71f75d8dc016127a93e6
Ubaid7 <75165174+Ubaid7@users.noreply.github.com>
2021-01-27 20:55:35 +0300

    Adding Basic Command Handler

---
+++ Episode 02/index.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.commands = new Discord.Collection();
+const fs = require('fs');
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`)
+    client.commands.set(command.name, command) 
+}
+
+const { prefix, token } = require('./config.json')
+
+client.once('ready', () => {
+    console.log('Ready.')
+})
+
+client.on('message', message => {
+    if(!message.content.startsWith(prefix)||message.author.bot) return
+
+    const args = message.content.slice(prefix.length).split(/ +/)
+    const command = args.shift().toLowerCase()
+
+    if(command === 'ping'){
+        client.commands.get('ping').execute(message, args)
+    } else if(command === 'yt'){
+        client.commands.get('yt').execute(message, args)
+    }
+})
+
+client.login(token)
