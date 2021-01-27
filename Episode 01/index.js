16b2e55298da6b28e69ca645fc388fda108fb369
Ubaid7 <75165174+Ubaid7@users.noreply.github.com>
2021-01-27 20:53:38 +0300

    Create index.js

---
+++ Episode 01/index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready.')
+})
+
+client.on('message', message => {
+	console.log(message.content); //If You Dont Want The Messages In Console Then Remove This Line
+  
+  if (message.content === 'ping') {
+	message.channel.send('Pong!'); //message.reply('Pong!'); Also If You Want The BOT To Ping The Person Who Used The Command
+} 
+  if (message.content === 'youtube') {
+	message.channel.send('https://youtube.com/techtipcyber'); //message.reply('https://youtube.com/techtipcyber'); Also If You Want The BOT To Ping The Person Who Used The Command
+} 
+
+});
