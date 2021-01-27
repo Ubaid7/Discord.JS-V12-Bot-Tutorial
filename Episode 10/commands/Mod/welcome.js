f39d415d467b8a3a92d129077ce10731f4fb4d57
Ubaid7 <75165174+Ubaid7@users.noreply.github.com>
2021-01-27 23:13:47 +0300

    Add files via upload

---
+++ Episode 10/commands/Mod/welcome.js
@@ -0,0 +1,23 @@
+module.exports = (client) => {
+    const welcomechannelId = '788270829581762580' //Channel You Want to Send The Welcome Message
+    const targetChannelId = `788271045660246077` //Channel For Rules
+
+    client.on('guildMemberAdd', (member) => {
+        console.log(member)
+        
+        const welocmemessage = ` <@${member.id}> Welcome To Our Server,
+Please Read ${member.guild.channels.cache.get(targetChannelId).toString()}
+Have A Nice Time!`
+        const channel = member.guild.channels.cache.get(welcomechannelId)
+        channel.send(welocmemessage)
+    })
+
+    const leavechannelId = '788272431868936194' //Channel You Want to Send The Leave Message
+
+    client.on('guildMemberRemove', (member) => {
+        const leavemessage = `<@${member.id}> Just Left Server.`
+
+        const channel1 = member.guild.channels.cache.get(leavechannelId)
+        channel1.send(leavemessage)
+    })
+}
