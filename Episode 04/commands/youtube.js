d171a632213555b6934f2f537871ff879f086b32
Ubaid7 <75165174+Ubaid7@users.noreply.github.com>
2021-01-27 21:21:15 +0300

    Create youtube.js

---
+++ Episode 04/commands/youtube.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'yt',
+    description: 'Sends YT Link', //Optional
+    execute(message, args) {
+        message.channel.send('https://youtube.com/techtipcyber')
+    }
+}
