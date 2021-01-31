ebdfa714ba00f8f15df2437e44be665df2c56ed3
Ubaid7 <75165174+Ubaid7@users.noreply.github.com>
2021-01-31 09:43:58 +0300

    Create ping.js

---
+++ Episode 12/commands/Fun/ping.js
@@ -0,0 +1,8 @@
+  
+module.exports = {
+    commands: ['p', 'ping'], // You Can Keep Any Name
+    description: 'Smple Ping Command', //Optional
+    callback: (message, args) => {
+        message.channel.send('Pong!')
+    }
+}
