6de96bcf6c32dd8ac646aa1649e6494670e0cf5f
Ubaid7 <75165174+Ubaid7@users.noreply.github.com>
2021-02-15 23:48:08 +0300

    Create youtube.js

---
+++ Episode 20/commands/Fun/youtube.js
@@ -0,0 +1,6 @@
+module.exports = {
+    commands: ['youtube', 'yt'], // You Can Keep Any Name
+    description: 'Sends YT Link', // Optional
+    callback: (message, args) => {
+        message.channel.send('https://youtube.com/techtipcyber')
+    }
