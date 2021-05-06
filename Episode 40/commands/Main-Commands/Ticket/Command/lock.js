4c75e7a539e2ed51b65a55136bca96bd7cc87c05
Ubaid7 <75165174+Ubaid7@users.noreply.github.com>
2021-05-06 17:06:09 +0300

    Lock Ticket Channel Command

---
+++ Episode 40/commands/Main-Commands/Ticket/Command/lock.js
@@ -0,0 +1,20 @@
+const db = require('quick.db') // npm i quick.db
+
+module.exports = {
+    commands: ['lock-ticket'], // You Can Keep Any Name
+    permissions: 'MANAGE_CHANNELS', // You Can Keep Any Permission
+    permissionError: 'You Cant Use This COmmand', // Optional
+    description: 'Lock Ticket', // Optional
+
+    callback: (message, args) => {
+
+        if(message.channel.name.includes('ticket')) return // If Channel Isn't Ticket Channel, Won't Work
+        else {
+            const memberinticket = db.fetch(`ticket-user_${message.channel.id}`) // Get User In Ticket
+            message.channel.updateOverwrite(memberinticket, {
+                SEND_MESSAGES: false
+            }) // Permission
+            message.channel.send(`Locked Ticket`)
+        }
+    }
+}
