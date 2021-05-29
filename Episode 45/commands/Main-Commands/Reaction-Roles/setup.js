842b5ee018a64682fae7216eab6a1261c50f60e7
Ubaid7 <75165174+Ubaid7@users.noreply.github.com>
2021-05-29 17:46:00 +0300

    Add files via upload

--- Episode 45/commands/Main-Commands/Reaction-Roles/setup.js
+++ Episode 45/commands/Main-Commands/Reaction-Roles/setup.js
@@ -1,1 +1,17 @@
+const { MessageEmbed } = require('discord.js')
+const EditMessage = require('../../../utils/EditMessage')
 
+module.exports = (client) => {
+    EditMessage(client, '811316961400193074', new MessageEmbed()
+    .setTitle('Reaction Roles')
+    .setColor('RED')
+    .setDescription(`**React**
+<:Yt:806408246733832232> React With To Get YouTube Role
+<:GitHub:811297109953347595> React With To Get GitHub Role
+<:VS:811297141669888040> React With To Get Visual Studio Role
+<:WikiPedia:811297151069323274> React With To Get WikiPedia Role
+    `)
+    .setFooter('Reaction Roles')
+    , ['<:Yt:806408246733832232>', '<:GitHub:811297109953347595>', '<:VS:811297141669888040>', '<:WikiPedia:811297151069323274>']
+    )
+}
