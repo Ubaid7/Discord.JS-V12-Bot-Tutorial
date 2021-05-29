54f8b159ad30447aec718cd10bebe22f8658df11
Ubaid7 <75165174+Ubaid7@users.noreply.github.com>
2021-05-29 17:50:56 +0300

    Add files via upload

--- Episode 45/commands/Per-Server-Commands/Reset/resetprefix.js
+++ Episode 45/commands/Per-Server-Commands/Reset/resetprefix.js
@@ -1,1 +1,18 @@
+const db = require('quick.db')
 
+module.exports = {
+    commands: ['reset-prefix'], // You Can Keep Any Name
+    description: 'ReSet Prefix For Server', // Optional
+    permissions: 'MANAGE_GUILD', // You Can Keep Any Permission
+    permissionError: 'You Dont Have Permission To Use This Command', // Optional
+
+    callback: async(message, args, client) => {
+
+        const prefix = db.fetch(`prefix_${message.guild.id}`) // Get Old Prefix
+        if(prefix === null) return message.reply('Prefix Not Set For This Server') // If No Prefix Is Set Yet
+        else if(prefix !== null) { // If Prefix Is Set Then...
+            message.reply('Prefix Reset To **+**')
+            db.delete(`prefix_${message.guild.id}`) // Delete Prefix And Set To Default Prefix
+        }
+    }
+}
