7288131369bbb8aa91ac78a5dca4687c8f20b6f5
Ubaid7 <75165174+Ubaid7@users.noreply.github.com>
2021-05-29 17:42:15 +0300

    Add files via upload

--- Episode 45/commands/Main-Commands/Economy/Shop/Buy/fish-rod.js
+++ Episode 45/commands/Main-Commands/Economy/Shop/Buy/fish-rod.js
@@ -3,28 +3,28 @@ const db = require('quick.db')
 
 module.exports = {
     commands: ['buy'], // You Can Keep Any Name
-    description: 'Buy Laptop', // Optional
+    description: 'Buy Fish Rod', // Optional
 
     callback: async(message, args, client) => {
 
-        if(args[0].toLowerCase() === 'laptop') { // You Can Keep Any Name
+        if(args[0].toLowerCase() === 'fish' && args[1].toLowerCase() === 'rod') { // You Can Keep Any Name
             const user = message.member
-            const amount = 10000 // Amount Of Laptop: $10,000
+            const amount = 15000 // Amount Of Fish Rod: $15,000
             const bal = db.fetch(`money_${user.id}`) // Get User's Money In Wallet
 
             if(bal <amount) { // If Balance In Wallet Is Less Then Amount Of Laptop
-                return message.reply(`You Don't Have Enough Money(\`$10,000\`) In Wallet To But Laptop`)
+                return message.reply(`You Don't Have Enough Money(\`$15,000\`) In Wallet To But Fish Rod`)
             } else {
                 const embed = new MessageEmbed()
                 .setAuthor(`${user.user.username} Purchased`, user.user.displayAvatarURL({ dynamic: true }))
                 .setTimestamp()
                 .setColor('RANDOM')
                 .setDescription(`
-<@${user.id}> Successfully Purchased **1** *Laptop* For \`$10,000\`
+<@${user.id}> Successfully Purchased **1** *Fish Rod* For \`$15,000\`
                 `)
                 .setFooter('Shop')
                 message.channel.send(embed)
-                db.add(`laptop_${user.id}`, 1) // Add 1 Laptop To User
+                db.add(`fishrod_${user.id}`, 1) // Add 1 Laptop To User
                 db.subtract(`money_${user.id}`, amount) // Remove Money From Using
             }
         }
