982323365747904e246d672b3f1eb2f6da710829
Ubaid7 <75165174+Ubaid7@users.noreply.github.com>
2021-05-29 17:42:41 +0300

    Add files via upload

--- Episode 45/commands/Main-Commands/Economy/Shop/Sell/fish-rod.js
+++ Episode 45/commands/Main-Commands/Economy/Shop/Sell/fish-rod.js
@@ -3,27 +3,27 @@ const db = require('quick.db')
 
 module.exports = {
     commands: ['sell'], // You Can Keep Any Name
-    description: 'Sell Laptop', // Optional
+    description: 'Sell Fish Rod', // Optional
 
     callback: async(message, args, client) => {
 
-        if(args[0].toLowerCase() === 'laptop') { // You Can Keep Any Name
+        if(args[0].toLowerCase() === 'fish' && args[1].toLowerCase() === 'rod') { // You Can Keep Any Name
             const user = message.member
-            const amount = 6000 // Amount Of Laptop To Sell: $6,000
-            const laptop = db.fetch(`laptop_${user.id}`) // Get User's Laptop 
-            if(laptop === null || laptop === 0) { // If User Doesn't Have Laptop
-                return message.reply(`You Don't Have Laptop To Sell It`)
-            } else if(laptop !== null || laptop !== 0) { // If User Has Laptop
+            const amount = 8000 // Amount Of Fish Rod To Sell: $8s,000
+            const fishrod = db.fetch(`fishrod_${user.id}`) // Get User's Fish Rod 
+            if(fishrod === null || fishrod === 0) { // If User Doesn't Have Fish Rod
+                return message.reply(`You Don't Have Fish Rod To Sell It`)
+            } else if(fishrod !== null || fishrod !== 0) { // If User Has Fish Rod
                 const embed = new MessageEmbed()
                 .setAuthor(`${user.user.username} Sold`, user.user.displayAvatarURL({ dynamic: true }))
                 .setTimestamp()
                 .setColor('RANDOM')
                 .setDescription(`
-<@${user.id}> Sold **1** *Laptop* For \`$6,000\`
+<@${user.id}> Sold **1** *Fish Rod* For \`$8,000\`
                 `)
                 .setFooter('Shop')
                 message.channel.send(embed)
-                db.subtract(`laptop_${user.id}`, 1) // Remove 1 Laptop From User
+                db.subtract(`fishrod_${user.id}`, 1) // Remove 1 Fish Rod From User
                 db.add(`money_${user.id}`, amount) // Add Money To User
             }
         }
