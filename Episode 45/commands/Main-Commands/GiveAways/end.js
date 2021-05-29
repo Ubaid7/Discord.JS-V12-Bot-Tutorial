4ae45d57ff9d83ea2463dd9a42e84b01f56b6773
Ubaid7 <75165174+Ubaid7@users.noreply.github.com>
2021-05-29 17:54:40 +0300

    End GiveAway Command

---
+++ Episode 45/commands/Main-Commands/GiveAways/end.js
@@ -0,0 +1,34 @@
+const { GiveawaysManager } = require('discord-giveaways') // npm i discord-giveaways
+const db = require('quick.db')
+module.exports = (client) => {
+    const manager = new GiveawaysManager(client, {
+        storage: '../../../giveaway.json', // Storing Data
+        updateCountdownEvery: 10000, // Update The Time In GiveAway Every 10 Seconds
+        default: {
+            botsCanWin: false, // Can Be true Also
+            embedColor: 'RED', // Color Of Embed
+            reaction: '🎉' // Reaction For Embed
+        }
+    })
+
+    client.giveawaysManager = manager
+    client.on('message', message => {
+        const prefix = db.fetch(`prefix_${message.guild.id}`) // You Can Do const prefix = '+' Also
+        const args = message.content.substring(prefix.lenght).split(' ')
+        switch (args[0]) {
+            case `${prefix}gend`: // Start GiveAway Command
+            if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(`You Don't Have Permission To Use This`) // If User Has No Permission
+
+            let messageID = args[1]
+            if(isNaN(messageID)) return message.reply(`Messsage ID Isn't A Number`) // If Message ID Isn't A Number
+            if(!messageID) return message.reply(`No Message ID Provided`) // If No Message ID Is Provided
+            client.giveawaysManager.edit(messageID, { // End GiveAway Using 'edit' Method
+                setEndTimestamp: Date.now() // End It After Some Seconds
+            }).then(() => {
+                message.channel.send(`GiveAway Successfully Ended, Will End In Some Seconds`)
+            }).catch((_err) => { // If Their Is Error...
+                message.reply(`No GiveAway With **${messageID}** Is Available`)
+            })
+        }
+    })
+}
