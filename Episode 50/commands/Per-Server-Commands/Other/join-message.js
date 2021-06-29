da2857cdc2073d8638bb1f66afc073680075ce28
Ubaid7 <75165174+Ubaid7@users.noreply.github.com>
2021-06-29 09:07:39 +0300

    Add files via upload

--- Episode 50/commands/Per-Server-Commands/Other/join-message.js
+++ Episode 50/commands/Per-Server-Commands/Other/join-message.js
@@ -1,1 +1,28 @@
+const { MessageEmbed } = require('discord.js')
+const db = require('quick.db')
 
+module.exports = (client) => {
+    client.on('guildCreate', (guild) => {
+        let prefix = db.fetch(`prefix_${guild.id}`) // Get prefix Of Server
+        if(!prefix || prefix === null) prefix = '+'
+
+        let joinmessagechannel
+        guild.channels.cache.forEach((channel) => { // Fetch Channel Which Is Text Channel And Bot Has Pemrission To Send Messsages
+            if(channel.type === 'text' && !joinmessagechannel && channel.permissionsFor(guild.me).has('SEND_MESSAGES')) 
+            joinmessagechannel = channel
+        })
+
+        if(!joinmessagechannel) return 
+
+        joinmessagechannel.send(
+            new MessageEmbed()
+            .setAuthor(guild.name, guild.iconURL({ dynamic: true }))
+            .setTimestamp()
+            .setColor('BLUE')
+            .setDescription(`
+My Prefix Is **${prefix}**
+Add Your Stuff Here...
+            `)
+        )
+    })
+}
